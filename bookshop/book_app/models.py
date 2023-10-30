9376fbe9890e804cf4c7c0efe1b46956d910cdcd
ZavbakGit <ag@799000.ru>
2023-10-30 19:55:37 +1000

    finish 4.6

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -7,5 +7,10 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return f'title = {self.title}, rating = {self.rating}'
+
+
+
 
 
