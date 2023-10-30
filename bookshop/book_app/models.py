2f1748b0751a511a350b3a3cfdb0e86b0a241ac7
ZavbakGit <ag@799000.ru>
2023-10-30 20:36:13 +1000

    finish 4.7

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -6,11 +6,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return f'title = {self.title}, rating = {self.rating}'
-
-
-
-
-
+        return (f'title = {self.title}, rating = {self.rating}, '
+                f'is_best_selling ={self.is_best_selling}, author ={self.author} ')
