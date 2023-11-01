fbd3a874d9343bce0cf9655a8ae1296ee43912de
ZavbakGit <ag@799000.ru>
2023-11-01 10:05:57 +1000

    finish 4.7

--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -8,6 +10,14 @@ class Movie(models.Model):
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=1000000)
+    slug = models.SlugField(default='', null=False, db_index=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Movie, self).save(*args, **kwargs)
+
+    def get_url(self):
+        return reverse('movie-detail', args=[self.slug])
 
     def __str__(self):
-        return f'name = {self.name}, rating = {self.rating}%'
+        return f'name = {self.name}, rating = {self.rating}%, year ={self.year}, budget ={self.budget}'
--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
-# Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    # for movie in movies:
+    #     movie.save()
+
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies
+    })
+
+
+def show_ont_movie(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
+    return render(request, 'movie_app/one_movie.html', {
+        'movie': movie
+    })
